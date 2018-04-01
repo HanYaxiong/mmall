1b062dd5ed3054cb24416b883aacfa9407bc5a34
King <85351649@qq.com>
2018-04-01 11:22:38 +0800

    test redis

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -5,6 +5,7 @@ public class Const {
 
     public static final String EMAIL = "email";
     public static final String USERNAME = "username";
+    public static final String TOKEN_PREFIX = "token_";
 
     public interface Role{
         int ROLE_CUSTOMER = 0;
